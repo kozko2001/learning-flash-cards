ac04233f7ee237b8b2f76e8b8c75e2969a60449e
Jordi <kozko2001@gmail.com>
2019-05-19 21:06:04 +0200

    ADD IAM user secret

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-s3`,
     {
       resolve: `gatsby-plugin-s3`,
       options: {
