321336d6e2db531ca6527344770c8550fcd8322b
Jordi <kozko2001@gmail.com>
2019-05-19 20:50:33 +0200

    add deploy to s3 in CI

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-s3`,
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: 'kzk-learning-flash-cards'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
